b8d2680e5956845f0682684fd08df5aa96720137
moatasm <hajjo.dev@gmail.com>
2024-03-09 23:04:16 +0100

    crud"admin"

--- src/index.php
+++ src/index.php
@@ -9,10 +9,17 @@ require dirname(__FILE__) . '/cls/router.php';
 if (isset($_GET["action"])) {
 	$action = $_GET["action"];
 	$fichier = redirect($action);
+} elseif (isset($_GET["table"])) {
+	$table = $_GET["table"];
+	$fichier = tableRedirect($table);
+
+	$fichier = 'admin/ReadCtrl.php';
 } else {
+
 	$fichier = 'accueil.controller.php';
 }
 
 require RACINE . "/controleur/" . $fichier;
 
-?>
+
+
